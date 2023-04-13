4e458c9d9003814b00b6f1a7bc6e111f950629a8
leandropaolo1 <coopergarciaandy@gmail.com>
2023-04-13 13:11:22 -0600

    commit

--- 4_introduction_to_neural_learning_ gradient_descent/4_one_iteration_of_gradient_descent.jl
+++ 4_introduction_to_neural_learning_ gradient_descent/4_one_iteration_of_gradient_descent.jl
@@ -21,6 +21,23 @@ function one_iteration_of_gradient_descent()
 
 end
 
+function one_iteration_of_gradient_descent()
+    input = 8.5
+    weight = 0.1
+    target = 1.0
+    alpha = 0.01
+
+    for iter in 1:10
+        pred = input * weight
+        error = (pred - target) ^ 2
+        weight =- (input * (pred-target)) * alpha
+
+        println("Iter: $iter Error: $(round(error, digits=5)) Prediction: $(round(pred, digits=5))")
+
+    end
+    
+end
+
 begin
     one_iteration_of_gradient_descent()
 end

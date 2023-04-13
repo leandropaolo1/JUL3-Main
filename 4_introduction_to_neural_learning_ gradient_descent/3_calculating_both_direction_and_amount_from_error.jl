30c722b126d5bf54db54b4f819b0f4c04faf2255
lpcooper <pcoper79@gmail.com>
2023-04-12 23:24:04 -0600

    commit

--- 4_introduction_to_neural_learning_ gradient_descent/3_calculating_both_direction_and_amount_from_error.jl
+++ 4_introduction_to_neural_learning_ gradient_descent/3_calculating_both_direction_and_amount_from_error.jl
@@ -2,6 +2,32 @@
 function calculating_both_direction_and_amount_from_error()
     weight = 0.5
     target = 0.8
+    input = 0.5
+    for iter in 1:300
+        pred = input * weight
+        error = (pred - target) ^ 2
+        direction_and_amount = (pred - target) * input
+        weight -= direction_and_amount
         
+        println("Error: $error Prediction: $pred")
+    end
+end
+
+function calculating_both_direction_and_amount_from_error()
+    target = 0.8
+    weight =  0.5
+    input = 0.5
+
+    for iter in 1:300
+        pred = input * weight
+        error = (pred - target) ^ 2
+        direction = (pred - target) * input
+        weight -= direction
 
 end
+
+begin
+    calculating_both_direction_and_amount_from_error()
+end
+
+

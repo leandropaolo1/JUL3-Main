0b162b445377bc6535ee9b55f6dc0d097eadf769
leandropaolo1 <coopergarciaandy@gmail.com>
2023-04-13 16:02:07 -0600

    COMMIT

--- 4_introduction_to_neural_learning_ gradient_descent/4_one_iteration_of_gradient_descent.jl
+++ 4_introduction_to_neural_learning_ gradient_descent/4_one_iteration_of_gradient_descent.jl
@@ -1,8 +1,9 @@
 # Grokking Deep Learning (Andrew W. Trask) (z-lib.org)
 # PDF pg. 59
 
+using Plots
 
-function one_iteration_of_gradient_descent()
+function first_iteration_of_gradient_descent()
     input = 8.5
     weight = 0.1
     target = 1.0
@@ -21,7 +22,7 @@ function one_iteration_of_gradient_descent()
 
 end
 
-function one_iteration_of_gradient_descent()
+function second_iteration_of_gradient_descent()
     input = 8.5
     weight = 0.1
     target = 1.0
@@ -38,6 +39,57 @@ function one_iteration_of_gradient_descent()
     
 end
 
+function third_iteration_of_gradient_descent()
+    input = 8.5
+    weight = 0.1
+    target = 1.0
+    alpha = 0.01
+    
+    iter_range = 1:10
+    
+    @gif for iter in iter_range
+        pred = input * weight
+        error = (pred - target) ^ 2
+        weight -= (input * (pred - target)) * alpha
+        
+        plt = plot([0, 10], [weight * 0, weight * 10], label="Prediction")
+        plot!([0, 10], [0, 10], label="Target")
+        plot!([input, input], [0, pred], linestyle=:dash, label="")
+        title!("Iteration: $iter, Error: $(round(error, digits=5))")
+        xlabel!("Input")
+        ylabel!("Output")
+    end
+end
+
+Base.@kwdef mutable struct GradDescent
+    input::Float64 = 8.5
+    weight::Float64 = 0.1
+    target::Float64 = 1.0
+    alpha::Float64 = 0.01
+    pred::Float64 = 0.0
+    error::Float64 = 0.0
+end
+
+function step!(l::GradDescent)
+    l.pred = l.input * l.weight
+    l.error = (l.pred - l.target) ^ 2
+    l.weight -= (l.input * (l.pred - l.target)) * l.alpha
+end
+
 begin
-    one_iteration_of_gradient_descent()
+    grad_descent = GradDescent()
+
+    plt = plot3d(
+        1,
+        xlim = (-1.0, 1.0),
+        ylim = (-1.0, 1.0),
+        title = "Gradual Descent",
+        legend = false,
+        marker = 2,
+    )
+
+    @gif for i=1:10
+        step!(grad_descent)
+        push!(plt, grad_descent.target, grad_descent.error)
+    end every 10
 end

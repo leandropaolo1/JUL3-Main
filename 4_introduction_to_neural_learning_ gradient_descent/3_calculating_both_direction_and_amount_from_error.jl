59e8ce8f3729eeecfc79475e1ec447ae5590d6b5
leandropaolo1 <coopergarciaandy@gmail.com>
2023-04-13 09:56:30 -0600

    commit

--- 4_introduction_to_neural_learning_ gradient_descent/3_calculating_both_direction_and_amount_from_error.jl
+++ 4_introduction_to_neural_learning_ gradient_descent/3_calculating_both_direction_and_amount_from_error.jl
@@ -1,9 +1,11 @@
+# Grokking Deep Learning (Andrew W. Trask) (z-lib.org)
+# PDF pg. 52
 
 function calculating_both_direction_and_amount_from_error()
     weight = 0.5
     target = 0.8
     input = 0.5
-    for iter in 1:300
+    for iter in 1:400
         pred = input * weight
         error = (pred - target) ^ 2
         direction_and_amount = (pred - target) * input
@@ -13,18 +15,6 @@ function calculating_both_direction_and_amount_from_error()
     end
 end
 
-function calculating_both_direction_and_amount_from_error()
-    target = 0.8
-    weight =  0.5
-    input = 0.5
-
-    for iter in 1:300
-        pred = input * weight
-        error = (pred - target) ^ 2
-        direction = (pred - target) * input
-        weight -= direction
-
-end
 
 begin
     calculating_both_direction_and_amount_from_error()
---
+++ 4_introduction_to_neural_learning_ gradient_descent/4_one_iteration_of_gradient_descent.jl
@@ -0,0 +1,20 @@
+function one_iteration_of_gradient_descent()
+    input = 8.5
+    weight = 0.1
+    target = 1.0
+    alpha = 0.01
+
+    for iter in 1:10
+        pred = input * weight
+        error = (pred - target) ^ 2
+        delta = pred - target
+        weighted_delta = input * delta
+        weight -= weighted_delta * alpha
+
+        println("Iter: $iter Error: $(round(error, digits=5)) Prediction: $(round(pred, digits=5))")
+    end
+end
+
+begin
+    one_iteration_of_gradient_descent()
+end

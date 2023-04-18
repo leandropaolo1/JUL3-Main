5f09f619fd5dd0f4f2eac822a0498316bc194e27
leandropaolo1 <coopergarciaandy@gmail.com>
2023-04-18 16:38:33 -0600

    commit

--- 5_learning_multiple_weights_at_a_time_generalizing_gradient_descent/2_gradient_descent_learning_with_multiple_outputs.jl
+++ 5_learning_multiple_weights_at_a_time_generalizing_gradient_descent/2_gradient_descent_learning_with_multiple_outputs.jl
@@ -5,20 +5,32 @@ Base.@kwdef mutable struct GradDescent
     alpha::Float64 = 0.01
     error::Float64 = 0.0
     delta::Float64 = 0.0
-    pred::Float64 = 0.0
 
     iter::Int16 = 1
     target = [1 1 0 1]
-    weights= [0.1 0.2 -0.1;] 
-    toes = [8.5 9.5 9.9 9.0]
-    nfans = [1.2 1.3 0.5 1.0]
-    wlrec = [0.65 0.8 0.8 0.9]
-    inputs = [toes;wlrec;nfans]
+    weights = [0.1 0.2 -0.1]
 
-    n_rows = size(weights,2) == 1 ? 1 : 1
-    weighted_deltas = zeros(1,3)
-    n_cols = size(inputs,2)
-
-    predictions = zeros(n_rows, n_cols)
+    inputs = [
+        8.5 9.5 9.9 9.0;
+        0.65 0.8 0.8 0.9;
+        1.2 1.3 0.5 1.0]
     
+    n_cols = size(inputs, 2)
+    n_rows = size(weights, 2) == 1 ? 1 : size(weights, 1)
+    weighted_deltas = zeros(1, 3)
+    pred = zeros(n_rows, n_cols)
+
+end
+
+
+function step!(grad::GradDescent, col::Int64)
+    for _ in 1:10
+        column = grad.inputs[:,col]
+        grad.pred = column' .* grad.weights
+
+
+
+
+    end
+
 end

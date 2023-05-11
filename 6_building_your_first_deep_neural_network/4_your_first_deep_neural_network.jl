9d42f6318a21f3be1f97f8cc6064e6bd189f16d7
leandropaolo1 <coopergarciaandy@gmail.com>
2023-05-11 16:12:56 -0600

    commit

--- 6_building_your_first_deep_neural_network/4_your_first_deep_neural_network.jl
+++ 6_building_your_first_deep_neural_network/4_your_first_deep_neural_network.jl
@@ -18,7 +18,7 @@ end
 
 Base.@kwdef mutable struct Layer
     iter::Int8 = 1
-    alpha::Float64 = 0.01
+    alpha::Float64 = 0.1
     hidden_size::Int16 = 4
     error::Float64 = 0.0
     pred::Matrix{Float64} = zeros(1,4)
@@ -27,13 +27,11 @@ Base.@kwdef mutable struct Layer
     weights_1_2::Matrix{Float64} = 2 * rand(hidden_size, 1) .- 1
 end
 
-input = StreetLight()
-
-function relu(layer::Layer)
+function rectified_linear_unit(layer::Layer)
     layer.pred = max.(layer.pred, 0)
 end
 
-function relu2deriv(layer::Layer)
+function rectified_linear_unit_derivative(layer::Layer)
     layer.pred = layer.pred .> 0
     pred = zeros(4,1)
     pred[:,1] = layer.pred
@@ -41,27 +39,26 @@ function relu2deriv(layer::Layer)
     
 end
 
+input = StreetLight()
 layer_0 = Layer()
 layer_1 = Layer()
 layer_2 = Layer()
 
-for iter in 1:60
-    total_error = 0.0
+for iter in 1:100
+    layer_2.error = 0
     for _iter in input.n_rows
 
-        layer_1.pred = input.inputs[_iter,:]' * layer_0.weights_0_1
-        relu(layer_1)
-        pred = layer_1.pred * layer_0.weights_1_2
-        layer_2.pred[1] = pred[1]
-        layer_2.error = sum((pred[1] .- input.inputs[_iter,:]) .^ 2)
-        total_error += layer_2.error
+        layer_1.pred = input.inputs[_iter : _iter,:] * layer_0.weights_0_1
+        rectified_linear_unit(layer_1)
+        pred = layer_1.pred * layer_0.weights_1_2 ; layer_2.pred[1] = pred[1]
+        layer_2.error += sum((layer_2.pred[1] .- input.inputs[_iter,:]) .^ 2)
         layer_2.delta[1] = layer_2.pred[1] - input.targets[_iter]
-        layer_1.delta[1,:] = layer_2.delta[1] .* layer_0.weights_1_2 .* relu2deriv(layer_1)
-        layer_0.weights_1_2 = layer_0.weights_1_2 .- (layer_0.alpha * layer_2.delta[1])
-        layer_0.weights_0_1 = layer_0.weights_0_1 .- (layer_0.alpha .* layer_1.delta[:,1])'
+        layer_1.delta = layer_2.delta[1] * layer_0.weights_1_2 .* rectified_linear_unit_derivative(layer_1)
+        layer_0.weights_1_2' .-= layer_0.alpha *layer_1.pred * layer_2.delta[1]
+        layer_0.weights_0_1 .-= layer_0.alpha * input.inputs[_iter : _iter,:]' * layer_1.delta'
     end
     if iter % 10 == 0
-        println("Iteration: $iter - Error: $total_error")
+        println("Iteration: $iter - Pred: $(layer_2.pred[1]) - Error: $(layer_2.error)")
     end
 
     

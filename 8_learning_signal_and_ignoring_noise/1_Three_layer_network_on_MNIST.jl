eef14817d0cdd0a7a9139918277b72b91e8423d5
leandropaolo1 <coopergarciaandy@gmail.com>
2023-05-24 16:53:30 -0600

    commit

--- 8_learning_signal_and_ignoring_noise/1_Three_layer_network_on_MNIST.jl
+++ 8_learning_signal_and_ignoring_noise/1_Three_layer_network_on_MNIST.jl
@@ -36,3 +36,20 @@ Base.@kwdef mutable struct Number
     weights_0_1::Matrix{Float64} = 2 * rand(pixels, hidden_size) .- 1
     weights_1_2::Matrix{Float64} = 2 * rand(hidden_size, num_labels ) .- 1
 end
+
+Base.@kwdef mutable struct Layer
+    # Learning rate.
+    alpha::Float64 = 0.1
+    # Size of the hidden layer.
+    hidden_size::Int16 = 40
+    # Error in prediction.
+    error::Float64 = 0.0
+    # Predicted output.
+    pred::Matrix{Float64} = zeros(1,4)
+    # Delta represents the gradient or derivative of the error.
+    delta::Matrix{Float64} = zeros(1,4)
+    # Weights for the connections between the input layer and the hidden layer.
+    weights_0_1::Matrix{Float64} = 2 * rand(3, hidden_size) .- 1
+    # Weights for the connections between the hidden layer and the output layer.
+    weights_1_2::Matrix{Float64} = 2 * rand(hidden_size, 1) .- 1
+end

8b52a3394a83c38d166eb1d24d9c20f33ac22dab
leandropaolo1 <coopergarciaandy@gmail.com>
2023-05-25 10:35:01 -0600

    commit

--- 8_learning_signal_and_ignoring_noise/1_Three_layer_network_on_MNIST.jl
+++ 8_learning_signal_and_ignoring_noise/1_Three_layer_network_on_MNIST.jl
@@ -17,39 +17,30 @@ single_image = MNIST(:train).features[:, :, 1]
 single_target = MNIST(:train).targets[1]
 
 
-function rectified_linear_unit(number::Number)
-
+function rectified_linear_unit(layer::Layer)
+    layer.pred = max.(layer.pred, 0)
 end
 
-function rectified_linear_unit_derivative(number::Number)
-
+function rectified_linear_unit_derivative(layer::Layer)
+    layer.pred = layer.pred .> 0
+    pred = zeros(4,1)
+    pred[:,1] = layer.pred
+    return pred
 end
 
 Base.@kwdef mutable struct Number
     possible_matches::Int64=10
-    hidden_size::Int64=40
-    alpha::Float64=0.005
     pixels::Int64=784
-    target::Int8
-    pred::Matrix{Float64} = zeros(1,hidden_size)
-    delta::Matrix{Float64} = zeros(1,hidden_size)
-    weights_0_1::Matrix{Float64} = 2 * rand(pixels, hidden_size) .- 1
-    weights_1_2::Matrix{Float64} = 2 * rand(hidden_size, num_labels ) .- 1
+    target::Int8=0
+
 end
 
 Base.@kwdef mutable struct Layer
-    # Learning rate.
-    alpha::Float64 = 0.1
-    # Size of the hidden layer.
-    hidden_size::Int16 = 40
-    # Error in prediction.
+    alpha::Float64=0.005
+    hidden_size::Int64=40
     error::Float64 = 0.0
-    # Predicted output.
     pred::Matrix{Float64} = zeros(1,4)
-    # Delta represents the gradient or derivative of the error.
     delta::Matrix{Float64} = zeros(1,4)
-    # Weights for the connections between the input layer and the hidden layer.
-    weights_0_1::Matrix{Float64} = 2 * rand(3, hidden_size) .- 1
-    # Weights for the connections between the hidden layer and the output layer.
-    weights_1_2::Matrix{Float64} = 2 * rand(hidden_size, 1) .- 1
+    weights_0_1::Matrix{Float64} = 2 * rand(pixels, hidden_size) .- 1
+    weights_1_2::Matrix{Float64} = 2 * rand(hidden_size, num_labels ) .- 1
 end
